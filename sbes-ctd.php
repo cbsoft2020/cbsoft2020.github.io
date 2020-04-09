75a65fcc8740f524181d142a261dce1b3e2afeee
Everton Cavalcante <evertonranielly@gmail.com>
2020-04-09 10:16:19 -0300

    Inclusão da página do CTD-ES

---
+++ sbes-ctd.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- HEAD -->
+<?php 
+	include_once ("header.php");
+?>
+
+<body class="body-wrapper">
+
+
+<!--========================================
+=            Navigation Section and Banner         =
+=========================================-->
+
+<?php 
+	include_once ("menu.php");
+?>
+
+<!--====  End of Navigation Section and Banner====-->
+
+<section class="section about">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 align-self-center">
+				<div class="content-block">
+                    <h2 data-i18n="ctd-es.titulo"></h2>
+					<h4 data-i18n="ctd-es.subtitulo"></h4>
+					
+					<br/>
+					<div class="description-one">
+						<p data-i18n="ctd-es.descricao_1"></p>
+					</div>
+					
+					<br/>
+					<h5 data-i18n="ctd-es.secao_premiacao"></h5>
+					<div class="description-one">
+						<p data-i18n="ctd-es.descricao_2"></p>
+					</div>
+					
+					<br/>
+					<h5 data-i18n="ctd-es.secao_publico-alvo"></h5>
+					<div class="description-one">
+						<p data-i18n="ctd-es.descricao_3"></p>
+					</div>
+					
+					<br/>
+					<h5 data-i18n="ctd-es.secao_submissao"></h5>
+					<div class="description-one">
+						<p style="margin-bottom: 10px" data-i18n="ctd-es.descricao_4"></p>
+						<p data-i18n="ctd-es.descricao_5"></p>
+						<ul style="margin-left: -20px">
+							<li data-i18n="ctd-es.documentos_submissao.0" style="color: black;"></li>
+							<li data-i18n="ctd-es.documentos_submissao.1" style="color: black;"></li>
+							<li data-i18n="ctd-es.documentos_submissao.2" style="color: black;"></li>
+						</ul>
+						<p data-i18n="ctd-es.descricao_6"></p>
+						<ul style="margin-left: -20px">
+							<li data-i18n="ctd-es.items_carta_orientador.0" style="color: black;"></li>
+							<li data-i18n="ctd-es.items_carta_orientador.1" style="color: black;"></li>
+							<li data-i18n="ctd-es.items_carta_orientador.2" style="color: black;"></li>
+							<li data-i18n="ctd-es.items_carta_orientador.3" style="color: black;"></li>
+						</ul>
+						<p data-i18n="ctd-es.descricao_7"></p>
+						<ul style="margin-left: -20px">
+							<li data-i18n="ctd-es.publicacao.0" style="color: black;"></li>
+							<li data-i18n="ctd-es.publicacao.1" style="color: black;"></li>
+						</ul>
+					</div>
+					
+					<br/>
+					<h5 data-i18n="ctd-es.secao_selecao"></h5>
+					<div class="description-one">
+						<p style="margin-bottom: 10px" data-i18n="ctd-es.descricao_8"></p>
+						<p style="margin-bottom: 10px" data-i18n="ctd-es.descricao_9"></p>
+						<p data-i18n="ctd-es.descricao_10"></p>
+					</div>
+					
+					<br/>
+					<h5 data-i18n="ctd-es.secao_datas"></h5>
+                    <div class="description-one">
+						<ul style="margin-left: -20px">
+							<li>
+								<span data-i18n="ctd-es.deadlines_atividades.0" style="color: black;"></span>:
+								<span data-i18n="ctd-es.deadlines.0" style="color: black;"></span>
+							</li>
+                            <li>
+								<span data-i18n="ctd-es.deadlines_atividades.1" style="color: black;"></span>:
+								<span data-i18n="ctd-es.deadlines.1" style="color: black;"></span>
+							</li>
+						</ul>
+					</div>
+					
+					<br/>
+					<h5 data-i18n="ctd-es.secao_comite"></h5>
+					<div class="description-one">
+						<strong><p data-i18n="ctd-es.comite.0"></p></strong>
+						<p data-i18n="ctd-es.comite.1"></p>
+						<p data-i18n="ctd-es.comite.2"></p>
+						<strong><p style="margin-top: 10px" data-i18n="ctd-es.comite.3"></p></strong>
+						<p data-i18n="ctd-es.comite.4"></p>
+						<p data-i18n="ctd-es.comite.5"></p>
+						<p data-i18n="ctd-es.comite.6"></p>
+						<p data-i18n="ctd-es.comite.7"></p>
+						<p data-i18n="ctd-es.comite.8"></p>
+						<p data-i18n="ctd-es.comite.9"></p>
+						<p data-i18n="ctd-es.comite.10"></p>
+						<p data-i18n="ctd-es.comite.11"></p>
+						<p data-i18n="ctd-es.comite.12"></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!--============================
+=            Footer            =
+=============================-->
+
+<?php 
+	include_once ("footer.php");
+?>
+</body>
+
+</html>
