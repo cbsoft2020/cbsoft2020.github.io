1116739ce847117a831a1c8c1bf0c3904b6e78fd
Everton Cavalcante <evertonranielly@gmail.com>
2020-04-09 09:15:26 -0300

    Adicionando fotos de Coordenadores

--- organizacao.php
+++ organizacao.php
@@ -192,6 +192,68 @@
 		<div class="row">
 			<div class="col-12">
 				<div class="sponsor-title text-center">
+					<h5 style="margin-bottom: 25px">SBES 2020 &ndash; <span data-i18n="ctic-es.subtitulo"></span></h5>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<?php foreach($data->chairs_sbes_ctic as $org) { ?>
+                <div class="col-lg-3 col-md-4 col-sm-6">
+                    <div class="speaker-item">
+                        <div class="image">
+                            <img src="images/organizadores/<?php echo $org->imagem;?>" alt="speaker" class="img-fluid">
+                            <div class="primary-overlay"></div>
+                            <div class="socials">
+                                <ul class="list-inline">
+                                    <li class="list-inline-item"><a href="<?php echo $org->lattes;?>"><img src="images/icon/lattes-2.png" style="padding-top: 8px;" width="40px" height="40px"></i></a></li>
+                                    <li class="list-inline-item"><a href="mailto:<?php echo $org->email;?>"><i class="fa fa-envelope "></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="content text-center">
+                            <p style="font-size: 18px"><?php echo $org->nome;?></p>
+                            <p style="font-size: 14px"><?php echo $org->universidade;?></p>
+                        </div>
+                    </div>
+                </div>
+			<?php } //foreach ?>
+		</div>
+
+		<br/>
+		<div class="row">
+			<div class="col-12">
+				<div class="sponsor-title text-center">
+					<h5 style="margin-bottom: 25px">SBES 2020 &ndash; <span data-i18n="ctd-es.subtitulo"></span></h5>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<?php foreach($data->chairs_sbes_ctd as $org) { ?>
+                <div class="col-lg-3 col-md-4 col-sm-6">
+                    <div class="speaker-item">
+                        <div class="image">
+                            <img src="images/organizadores/<?php echo $org->imagem;?>" alt="speaker" class="img-fluid">
+                            <div class="primary-overlay"></div>
+                            <div class="socials">
+                                <ul class="list-inline">
+                                    <li class="list-inline-item"><a href="<?php echo $org->lattes;?>"><img src="images/icon/lattes-2.png" style="padding-top: 8px;" width="40px" height="40px"></i></a></li>
+                                    <li class="list-inline-item"><a href="mailto:<?php echo $org->email;?>"><i class="fa fa-envelope "></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="content text-center">
+                            <p style="font-size: 18px"><?php echo $org->nome;?></p>
+                            <p style="font-size: 14px"><?php echo $org->universidade;?></p>
+                        </div>
+                    </div>
+                </div>
+			<?php } //foreach ?>
+		</div>
+
+		<br/>
+		<div class="row">
+			<div class="col-12">
+				<div class="sponsor-title text-center">
 					<h5 style="margin-bottom: 25px">SBCARS 2020</h5>
 				</div>
 			</div>
