4371ba4a3a3b28734f0cc33da9aa31e09d1ff0c5
Everton Cavalcante <evertonranielly@gmail.com>
2020-05-26 17:03:52 -0300

    Enquadramento do link da Trilha da Indústria no primeiro nível de menu

--- menu.php
+++ menu.php
@@ -1,97 +1,99 @@
 <nav class="navbar main-nav border-less fixed-top navbar-expand-lg p-0">
     <div class="container-fluid p-0">
         <a class="navbar-brand" href="index.php">
-          <img src="images/logo_lateral.png" width="100%" alt="logo">
+        	<img src="images/logo_lateral.png" width="100%" alt="logo">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         	<span class="fa fa-bars"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item dropdown active dropdown-slide">
-            <a class="nav-link" href="#"  data-toggle="dropdown">CBSoft 2020</a>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="organizacao.php" data-i18n="menu.organizacao">Organização</a>
-              <a class="dropdown-item" href="patrocinio.php" data-i18n="menu.patrocinio">Patrocínio</a>
-              <a class="dropdown-item" href="index.php#programacao" data-i18n="menu.programacao">Programação</a>
-              <a class="dropdown-item" href="#" data-i18n="menu.anais">Anais</a>
-              <a class="dropdown-item" href="trilha-industria.php" data-i18n="menu.trilha_industria">Trilha da Indústria</a>
-              <a class="dropdown-item" href="#" data-i18n="menu.tutoriais">Tutoriais</a>
-              <a class="dropdown-item" href="#" data-i18n="menu.minicursos">Minicursos</a>
-              <a class="dropdown-item" href="wtdsoft.php" data-i18n="menu.wtdsoft">Workshop de Teses e Dissertações</a>
-              <a class="dropdown-item" href="codigo-conduta.php" data-i18n="menu.codigo_conduta">Código de Conduta</a>
-              <a class="dropdown-item" href="edicoes-anteriores.php" data-i18n="menu.edicoes_anteriores">Edições Anteriores</a>
-            </div>
-          </li>
-          <li class="nav-item dropdown dropdown-slide">
-            <a class="nav-link" href="sbes.php" data-toggle="dropdown">SBES</a>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="sbes.php" data-i18n="menu.home_SBES">Página Principal</a>
-              <a class="dropdown-item" href="sbes-pesquisa.php" data-i18n="menu.trilha_pesquisa">Trilha de Pesquisa</a>
-			  <a class="dropdown-item" href="sbes-ideias.php" data-i18n="menu.trilha_ideias_inovadoras">Trilha de Ideias Inovadoras e Resultados Emergentes</a>
-              <a class="dropdown-item" href="sbes-educacao.php" data-i18n="menu.trilha_educacao">Trilha de Educação</a>
-			  <a class="dropdown-item" href="sbes-ferramentas.php" data-i18n="menu.trilha_ferramentas">Trilha de Ferramentas</a>
-			  <a class="dropdown-item" href="sbes-ctic.php" data-i18n="menu.ctic-es">Concurso de Trabalhos de Iniciação Científica</a>
-			  <a class="dropdown-item" href="sbes-ctd.php" data-i18n="menu.ctd-es">Concurso de Teses e Dissertações</a>
-              <a class="dropdown-item" href="sbes.php" data-i18n="menu.palestras">Palestras</a>
-            </div>
-          </li>
-          <li class="nav-item dropdown dropdown-slide">
-              <a class="nav-link" href="sbcars.php" data-toggle="dropdown">SBCARS</a>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="sbcars.php" data-i18n="menu.chamada_trabalhos">Chamada de Trabalhos</a>
-                <a class="dropdown-item" href="sbcars.php" data-i18n="menu.artigos_aceitos">Artigos Aceitos</a>
-                <a class="dropdown-item" href="sbcars.php" data-i18n="menu.palestras">Palestras</a>
-              </div>
-          </li>
-          <li class="nav-item dropdown dropdown-slide">
-              <a class="nav-link" href="sblp.php" data-toggle="dropdown">SBLP</a>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="sblp.php" data-i18n="menu.chamada_trabalhos">Chamada de Trabalhos</a>
-                <a class="dropdown-item" href="sblp.php" data-i18n="menu.artigos_aceitos">Artigos Aceitos</a>
-                <a class="dropdown-item" href="sblp.php" data-i18n="menu.palestras">Palestras</a>
-              </div>
-          </li>
-          <li class="nav-item dropdown dropdown-slide">
-              <a class="nav-link" href="sast.php" data-toggle="dropdown">SAST</a>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="sast.php" data-i18n="menu.chamada_trabalhos">Chamada de Trabalhos</a>
-                <a class="dropdown-item" href="sast.php" data-i18n="menu.artigos_aceitos">Artigos Aceitos</a>
-                <a class="dropdown-item" href="sast.php" data-i18n="menu.palestras">Palestras</a>
-              </div>
-          </li>
-          <li class="nav-item dropdown dropdown-slide">
-              <a class="nav-link" href="workshops.php">Workshops</a>
-              <div class="dropdown-menu">
-				  <a class="dropdown-item" href="https://ww2.inf.ufg.br/mssis/" target="_blank" data-i18n="workshops.mssis">MSSiS</a>
-                  <a class="dropdown-item" href="https://vem2020.github.io/vem2020/" target="_blank" data-i18n="workshops.vem">VEM</a>
-              </div>
-          </li>
-		  <!--
-          <li class="nav-item dropdown dropdown-slide">
-              <a class="nav-link" href="#" data-i18n="menu.informacoes">Informações</a>
-              <div class="dropdown-menu">
-                  <a class="dropdown-item" href="local.php" data-i18n="menu.local">Local do Evento</a>
-				  <a class="dropdown-item" href="sobre-natal.php" data-i18n="menu.natal">Sobre Natal</a>
-                  <a class="dropdown-item" href="informacoes-viagem.php" data-i18n="menu.infoViagem">Informações de Viagem</a>
-                  <a class="dropdown-item" href="hospedagem.php" data-i18n="menu.hospedagem">Hospedagem</a>
-              </div>
-          </li>
-		  -->
-          <li class="nav-item">
-              <a class="nav-link" href="contato.php" data-i18n="menu.contato">Contato</a>
-          </li>
-          <li class="nav-item">
-              <form style="margin-right: 5px">
-                  <input id="toggleImage" type="image" name="lang" value="pt" src="images/icon/en.png" onclick="toggleImage(this); return false;" 
-              style="margin-top: 10px; margin-left: 5px; margin-right: 36px;" width="40"/>
-            </form>
-          </li>
-        </ul>
-        <a href="#" class="ticket">
-          <span style="color: #ffffff"><i class="fa fa-ticket fa-lg"></i></span>&nbsp;
-          <span data-i18n="menu.inscricoes">Inscreva-se</span>
-        </a>
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item dropdown active dropdown-slide">
+                    <a class="nav-link" href="#"  data-toggle="dropdown">CBSoft 2020</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="organizacao.php" data-i18n="menu.organizacao">Organização</a>
+                        <a class="dropdown-item" href="patrocinio.php" data-i18n="menu.patrocinio">Patrocínio</a>
+                        <a class="dropdown-item" href="index.php#programacao" data-i18n="menu.programacao">Programação</a>
+                        <a class="dropdown-item" href="#" data-i18n="menu.anais">Anais</a>
+                        <a class="dropdown-item" href="#" data-i18n="menu.tutoriais">Tutoriais</a>
+                        <a class="dropdown-item" href="#" data-i18n="menu.minicursos">Minicursos</a>
+                        <a class="dropdown-item" href="codigo-conduta.php" data-i18n="menu.codigo_conduta">Código de Conduta</a>
+                        <a class="dropdown-item" href="edicoes-anteriores.php" data-i18n="menu.edicoes_anteriores">Edições Anteriores</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown dropdown-slide">
+                    <a class="nav-link" href="sbes.php" data-toggle="dropdown">SBES</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="sbes.php" data-i18n="menu.home_SBES">Página Principal</a>
+                        <a class="dropdown-item" href="sbes-pesquisa.php" data-i18n="menu.trilha_pesquisa">Trilha de Pesquisa</a>
+                        <a class="dropdown-item" href="sbes-ideias.php" data-i18n="menu.trilha_ideias_inovadoras">Trilha de Ideias Inovadoras e Resultados Emergentes</a>
+                        <a class="dropdown-item" href="sbes-educacao.php" data-i18n="menu.trilha_educacao">Trilha de Educação</a>
+                        <a class="dropdown-item" href="sbes-ferramentas.php" data-i18n="menu.trilha_ferramentas">Trilha de Ferramentas</a>
+                        <a class="dropdown-item" href="sbes-ctic.php" data-i18n="menu.ctic-es">Concurso de Trabalhos de Iniciação Científica</a>
+                        <a class="dropdown-item" href="sbes-ctd.php" data-i18n="menu.ctd-es">Concurso de Teses e Dissertações</a>
+                        <a class="dropdown-item" href="sbes.php" data-i18n="menu.palestras">Palestras</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown dropdown-slide">
+                    <a class="nav-link" href="sbcars.php" data-toggle="dropdown">SBCARS</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="sbcars.php" data-i18n="menu.chamada_trabalhos">Chamada de Trabalhos</a>
+                        <a class="dropdown-item" href="sbcars.php" data-i18n="menu.artigos_aceitos">Artigos Aceitos</a>
+                        <a class="dropdown-item" href="sbcars.php" data-i18n="menu.palestras">Palestras</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown dropdown-slide">
+                    <a class="nav-link" href="sblp.php" data-toggle="dropdown">SBLP</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="sblp.php" data-i18n="menu.chamada_trabalhos">Chamada de Trabalhos</a>
+                        <a class="dropdown-item" href="sblp.php" data-i18n="menu.artigos_aceitos">Artigos Aceitos</a>
+                        <a class="dropdown-item" href="sblp.php" data-i18n="menu.palestras">Palestras</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown dropdown-slide">
+                    <a class="nav-link" href="sast.php" data-toggle="dropdown">SAST</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="sast.php" data-i18n="menu.chamada_trabalhos">Chamada de Trabalhos</a>
+                        <a class="dropdown-item" href="sast.php" data-i18n="menu.artigos_aceitos">Artigos Aceitos</a>
+                        <a class="dropdown-item" href="sast.php" data-i18n="menu.palestras">Palestras</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="trilha-industria.php" data-i18n="menu.trilha_industria">Trilha da Indústria</a>
+                </li>
+                <li class="nav-item dropdown dropdown-slide">
+                    <a class="nav-link" href="workshops.php">Workshops</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="https://ww2.inf.ufg.br/mssis/" target="_blank" data-i18n="workshops.mssis">MSSiS</a>
+                        <a class="dropdown-item" href="https://vem2020.github.io/vem2020/" target="_blank" data-i18n="workshops.vem">VEM</a>
+						<a class="dropdown-item" href="wtdsoft.php" data-i18n="menu.wtdsoft">WTDSoft</a>
+                    </div>
+                </li>
+                <!--
+                <li class="nav-item dropdown dropdown-slide">
+                	<a class="nav-link" href="#" data-i18n="menu.informacoes">Informações</a>
+                    <div class="dropdown-menu">
+                   		<a class="dropdown-item" href="local.php" data-i18n="menu.local">Local do Evento</a>
+                    	<a class="dropdown-item" href="sobre-natal.php" data-i18n="menu.natal">Sobre Natal</a>
+                        <a class="dropdown-item" href="informacoes-viagem.php" data-i18n="menu.infoViagem">Informações de Viagem</a>
+                        <a class="dropdown-item" href="hospedagem.php" data-i18n="menu.hospedagem">Hospedagem</a>
+                    </div>
+                </li>
+                -->
+                <li class="nav-item">
+                    <a class="nav-link" href="contato.php" data-i18n="menu.contato">Contato</a>
+                </li>
+                <li class="nav-item">
+                    <form style="margin-right: 5px">
+                        <input id="toggleImage" type="image" name="lang" value="pt" src="images/icon/en.png" onclick="toggleImage(this); return false;" 
+                            style="margin-top: 10px; margin-left: 5px; margin-right: 36px;" width="40"/>
+                    </form>
+                </li>
+            </ul>
+            <a href="#" class="ticket">
+				<span style="color: #ffffff"><i class="fa fa-ticket fa-lg"></i></span>&nbsp;
+				<span data-i18n="menu.inscricoes">Inscreva-se</span>
+            </a>
         </div>
     </div>
-  </nav>
+</nav>
